2cc223b8bc0d1e2dfcf514877308d5bb2cf6b24c
Azooz <asnobi@gmail.com>
2015-11-08 19:11:25 -0600

    Time Out and File transfare

---
+++ client/client.py
@@ -0,0 +1,60 @@
+import os
+from HTMLParser import HTMLParser
+from socket import *
+import time
+import sys
+import struct
+import thread
+import threading
+
+
+serverName = '127.0.0.1'
+serverPort = 14000
+
+newline = "\r\n\r\n"
+my_list = []
+file_requested = "index.txt"
+
+#sentence = ""
+addr = ""
+reply = ""
+
+sentence = "Hello, From Client"
+clientSocket = socket(AF_INET, SOCK_DGRAM)
+sentence = "GET " + file_requested + " HTTP/1.1%s" % (newline)
+#print sentence
+clientSocket.settimeout(5)
+
+i = 0
+#while i < 5:
+#	try:
+		#sentence = raw_input('Client : ')
+clientSocket.sendto(sentence, (serverName, serverPort))
+try:
+	reply, addr = clientSocket.recvfrom(1000)
+	#print reply
+	i = i + 1
+	MSG = 'Enter your message to send here : '
+except timeout:
+	print "time is out ..."
+	reply = "Nothing.."
+
+dirr = os.path.dirname(file_requested)
+#print dirr
+#os.stat(dirr)
+output_file = open(file_requested, 'w')
+output_file.write(reply)
+output_file.close()
+
+#	except clientSocket.error:
+#		print "Error timeout..."
+print "out..."
+		
+        
+
+
+
+#clientSocket.sendto(sentence, (serverName, serverPort))
+
+
+clientSocket.close()
--- server/server.py
+++ server/server.py
@@ -15,11 +15,21 @@ serverPort = 14000
 serverSocket = socket(AF_INET, SOCK_DGRAM)
 serverSocket.bind((serverName, serverPort))
 
+
+
 while True:
 	print "Server is ready to receive..."
-	MSG, addr = serverSocket.recvfrom(1024)
+	MSG, addr = serverSocket.recvfrom(1000)
 	print "received message is : ", MSG
-	reply = raw_input('Server : ')
-	serverSocket.sendto(reply , addr)
+	method = MSG.split(' ')[0]
+	my_file = MSG.split(' ')
+	my_file = my_file[1] 
+	print "file is :" + my_file
+	open_file = open(my_file,'rb')
+	if (method == 'GET'):
+		file_content = open_file.read() 
+	open_file.close()
+
+	serverSocket.sendto(file_content , addr)
 
 serverSocket.close()
